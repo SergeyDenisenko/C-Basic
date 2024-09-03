501f94d17fe984dd238bef57a73c8bab42f967cc
Sergey <sergey.d91@mail.ru>
2024-09-03 18:45:58 +0300

    create second version output hello world

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
 int main() {
+    // Легко и непринуждённо.
     std::cout << "=========================\r\n";
     std::cout << "Hello,\r\nWorld\r\n";
     std::cout << "=========================\r\n";
+
+    // Красиво.
+    std::cout << "=========================\r\n";
+    std::cout << "||        Hello,       ||\r\n";
+    std::cout << "||        World        ||\r\n";
+    std::cout << "=========================\r\n";
+
+
 }
